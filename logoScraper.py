76ee86d6b71233f20b141890109d9e407a2cf0f3
Andrew Bass <andrewbass@Andrews-MBP.hsd1.fl.comcast.net>
2021-06-06 11:16:44 -0400

    Working logoScraper.py

--- logoScraper.py
+++ logoScraper.py
@@ -12,11 +12,20 @@ capdURL = "https://www.politicsanddesign.com/"
 #TODO: change this so the function searches machine for chromedriver
 driver = webdriver.Chrome(executable_path="/Users/andrewbass/Desktop/bad_answers/Tech_Projects/chromedriver")
 
-
 driver.get(capdURL)
-time.sleep(10)
+time.sleep(3)
 
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+bottom_height = driver.execute_script("return document.body.scrollHeight")
+
+#Loop to scroll and dynamically load the page
+while True:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    #Wait to load
+    time.sleep(1)
+    new_bottom = driver.execute_script("return document.body.scrollHeight")
+    if new_bottom == bottom_height:
+        break
+    bottom_height = new_bottom
 
 page_source = driver.page_source
 soup = BeautifulSoup(page_source, 'html.parser')
@@ -27,12 +36,10 @@ images = imageGallery.findAll("img")
 
 print(len(images))
 
-# #Downloads images using src URL
-# for idx, image in enumerate(images):
-#     imageURL = "https:" + image.attrs['src'].split(" ", 1)[0]
-#     img = Image.open(requests.get(imageURL, stream=True).raw)
-#     img.save('/Users/andrewbass/Desktop/bad_answers/tech_projects/political-logo-GAN/data/' + str(idx) + '.jpg', 'JPEG')
+#Downloads images using src URL
+for idx, image in enumerate(images):
+    imageURL = "https:" + image.attrs['src'].split(" ", 1)[0]
+    img = Image.open(requests.get(imageURL, stream=True).raw)
+    img.save('/Users/andrewbass/Desktop/bad_answers/tech_projects/political-logo-GAN/data/' + str(idx) + '.jpg', 'JPEG')
 
-# driver.quit()
-
-# print("AAAAAAAAAAAAA")
+driver.quit()
